fc82004b6bcab8db74e7a0a92cd83c5a293ea93e
saf.vadim <saf.vadim@hotmail.com>
2025-01-25 14:25:56 +0300

    Update to net9 Change swagger to openAPI

--- src/SiteAvailabilityMonitoring.Api/Program.cs
+++ src/SiteAvailabilityMonitoring.Api/Program.cs
@@ -1,19 +1,41 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SiteAvailabilityMonitoring.Api.Extensions;
+using Scalar.AspNetCore;
+using SiteAvailabilityMonitoring.Api.BackgroundServices;
+using SiteAvailabilityMonitoring.Api.Options;
+using SiteAvailabilityMonitoring.DataAccess;
+using SiteAvailabilityMonitoring.DataAccess.Implementations;
+using SiteAvailabilityMonitoring.Domain;
+using SiteAvailabilityMonitoring.Domain.DataAccessPoint;
 
-namespace SiteAvailabilityMonitoring.Api;
+var builder = WebApplication.CreateBuilder();
 
-public class Program
+builder.Services.AddOpenApi();
+
+builder.Services
+    .ConfigureDatabase(builder.Configuration)
+    .ConfigureMediatr();
+
+builder.Services
+    .AddSingleton<IWebsiteRepository, WebsiteRepository>()
+    .AddHttpClient<CheckWebsiteAvailabilityClient>();
+
+builder.Services.Configure<CheckerOptions>(builder.Configuration.GetSection("CheckerOptions"));
+
+builder.Services.AddHostedService<CheckerBackgroundService>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().ApplyMigrate().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
+
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
